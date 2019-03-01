95d9640436a384a8f4136c3822cbef2c4c23bfdb
sally <sallyakrum83@gmail.com>
2019-02-28 19:55:26 -0600

    adding changes

--- bamazon.sql
+++ bamazon.sql
@@ -1,8 +1,9 @@
-DROP DATABASE IF EXISTS bamazon_DB;
-CREATE DATABASE bamazon_DB;
-USE greatBay_DB;
+#DROP DATABASE IF EXISTS bamazon_DB;
+#CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
 CREATE TABLE products(
- item_id INT(10) NOT NULL ,
+ item_id INT(10) NOT NULL auto_increment,
  product_name VARCHAR(100) NOT NULL,
  department_name VARCHAR(45) NOT NULL,
  price INT default 0,
